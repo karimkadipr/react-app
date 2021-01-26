b76e7dec318c3205496443687e34194b37c6b39b
unknown <kadikarimpr@gmail.com>
2021-01-26 11:46:04 +0100

    new changes

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,10 +1,28 @@
-import React , {useEffect} from 'react';
+import React , {useEffect ,useState} from 'react';
 import './Description.css';
 import logo from './logo.jpg'
 import Aos  from 'aos';
 import 'aos/dist/aos.css';
 
 function Description() {
+    const [dimensions, setDimensions] = useState({ 
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+      useEffect(() => {
+        function handleResize() {
+          setDimensions({
+            height: window.innerHeight,
+            width: window.innerWidth
+          })
+        }
+          window.addEventListener('resize', handleResize)
+             return _ => {
+          window.removeEventListener('resize', handleResize)
+          
+      }
+      })
+
     useEffect(() => {
         Aos.init({});
     }, [])
@@ -16,8 +34,9 @@ function Description() {
                 <h1>I'm Karim,</h1>
                 <h1>web developer</h1>
                 <h2> - Front end web developer - </h2>
-            </div>    
-            <img  className="image" data-aos-once="true" data-aos="fade-left" data-aos-duration="2000" src={logo} className="image" alt="Nothing to show"/>
+            </div>   
+            {dimensions.width > 1000 && <img  className="image" data-aos-once="true" data-aos="fade-left" data-aos-duration="2000" src={logo} className="image" alt="Nothing to show"/>}
+            
         </div>
         
         
